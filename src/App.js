388c9e90e1ab9f229699712afa6442e21f6c1d76
aravind <naravin1@ford.com>
2025-01-08 12:54:33 +0530

    complete booking form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
 import React from 'react';
-import Header from './Header';
 import Nav from './Nav';
 import Main from './Main';
-import Footer from './Footer';
 
 
 function App() {
   return (
     <>
      <Nav/>
-     <Header/>
      <Main/>
-     <Footer/>
     </>
   );
 }
