8606b9d483485128b1c31d6a5df177001e51271e
aravind <naravin1@ford.com>
2025-01-08 09:23:37 +0530

    nav bar changes

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import logo from './images/Logo.svg'
 function Nav() { 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
     return (
-        <nav>
-            <a href="/">
+
+        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+            <a href="/" className="logo">
             <img src={logo} alt="logo" />
             </a>
-            <div>
+            <div className="menu-icon" onClick={toggleMenu}>
                 <div className='bar'></div>
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
 
-            <ul>
+            <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
                 <li>
                     <a href='/'>Home</a>
                 </li>
